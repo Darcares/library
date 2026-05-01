2381617904d248b0eb55a4c453c1af5ec733df76
Daniel Cárdenas <58755748+Darcares@users.noreply.github.com>
2026-05-01 02:25:29 +0100

    feat: Encapsule logic

--- script.js
+++ script.js
@@ -1,102 +1,106 @@
 "use strict";
 
-const library = [];
-const form = document.querySelector("#form");
-const container = document.querySelector(".container");
+initialize();
 
-form.addEventListener("submit", (event) => {
+function initialize() {
+    const library = [];
+    const form = document.querySelector("#form");
+    const container = document.querySelector(".container");
 
-    event.preventDefault();
-    const bookData = getFormData(form);
-    const newBook = new Book(bookData);
-    library.push(newBook);
-    printLibrary();
-    form.reset();
-});
+    form.addEventListener("submit", (event) => {
 
-container.addEventListener("click", (event) => {
+        event.preventDefault();
+        const bookData = getFormData(form);
+        const newBook = new Book(bookData);
+        library.push(newBook);
+        printLibrary();
+        form.reset();
+    });
 
-    const buttonClass = event.target.className;
-    const bookID = event.target.closest(".card").dataset.id;
+    container.addEventListener("click", (event) => {
 
-    switch(buttonClass) {
+        const buttonClass = event.target.className;
+        const bookID = event.target.closest(".card").dataset.id;
 
-        case "toggle": 
-            const book = library.find(element => element.id === bookID);
-            book.toggleReadStatus();
-            printLibrary();
-            break;
+        switch(buttonClass) {
 
-        case "delete":
-            const bookIndex = library.findIndex(element => element.id === bookID);
-            library.splice(bookIndex, 1);
-            printLibrary();
-            break;
+            case "toggle": 
+                const book = library.find(element => element.id === bookID);
+                book.toggleReadStatus();
+                printLibrary();
+                break;
 
-        default:
-            console.log("Error, no case");
-            break;
+            case "delete":
+                const bookIndex = library.findIndex(element => element.id === bookID);
+                library.splice(bookIndex, 1);
+                printLibrary();
+                break;
+
+            default:
+                console.log("Error, no case");
+                break;
+        }
+    });
+
+    function Book(bookData) {
+
+        this.name = bookData.get("name");
+        this.author = bookData.get("author");
+        this.pages = bookData.get("pages");
+        this.read = bookData.get("read") === "on" ? true : false;
+        this.id = crypto.randomUUID();
     }
-});
 
-function Book(bookData) {
+    Book.prototype.toggleReadStatus = function () {
 
-    this.name = bookData.get("name");
-    this.author = bookData.get("author");
-    this.pages = bookData.get("pages");
-    this.read = bookData.get("read") === "on" ? true : false;
-    this.id = crypto.randomUUID();
+        this.read = !this.read;
+    }
+
+    function getFormData(form) {
+
+        const bookData = new FormData(form);
+        return bookData;
+    }
+
+    function createCard(book) {
+
+        const card = document.createElement("div");
+        card.className = "card";
+        card.dataset.id = book.id;
+
+        const name = document.createElement("p");
+        const author = document.createElement("p");
+        const pages = document.createElement("p");
+        const read = document.createElement("p");
+        const toggle = document.createElement("button");
+        const del = document.createElement("button");
+
+        toggle.className = "toggle";
+        del.className = "delete";
+
+        name.textContent = `Name: ${book.name}`;
+        author.textContent = `Author: ${book.author}`;
+        pages.textContent = `Pages: ${book.pages}`;
+        read.textContent = `Read?: ${book.read === true ? "Yes" : "No"}`;
+        toggle.textContent = book.read === true ? "Not read" : "Read";
+        del.textContent = "Delete";
+
+        card.appendChild(name);
+        card.appendChild(author);
+        card.appendChild(pages);
+        card.appendChild(read);
+        card.appendChild(toggle);
+        card.appendChild(del);
+
+        container.appendChild(card);
+    }
+
+    function printLibrary() {
+
+        container.innerHTML = "";
+
+        library.forEach(book => {
+            createCard(book);
+        });   
+    }   
 }
-
-Book.prototype.toggleReadStatus = function () {
-
-    this.read = !this.read;
-}
-
-function getFormData(form) {
-
-    const bookData = new FormData(form);
-    return bookData;
-}
-
-function createCard(book) {
-
-    const card = document.createElement("div");
-    card.className = "card";
-    card.dataset.id = book.id;
-
-    const name = document.createElement("p");
-    const author = document.createElement("p");
-    const pages = document.createElement("p");
-    const read = document.createElement("p");
-    const toggle = document.createElement("button");
-    const del = document.createElement("button");
-
-    toggle.className = "toggle";
-    del.className = "delete";
-
-    name.textContent = `Name: ${book.name}`;
-    author.textContent = `Author: ${book.author}`;
-    pages.textContent = `Pages: ${book.pages}`;
-    read.textContent = `Read?: ${book.read === true ? "Yes" : "No"}`;
-    toggle.textContent = book.read === true ? "Not read" : "Read";
-    del.textContent = "Delete";
-
-    card.appendChild(name);
-    card.appendChild(author);
-    card.appendChild(pages);
-    card.appendChild(read);
-    card.appendChild(toggle);
-    card.appendChild(del);
-
-    container.appendChild(card);
-}
-
-function printLibrary() {
-
-    container.innerHTML = "";
-
-    library.forEach(book => {
-        createCard(book);
-    });   
-}   
